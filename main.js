bc5053d53659e1d407d501dbd7346c48912dc9e2
AN908 <159957994+AN908@users.noreply.github.com>
2024-03-19 13:09:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// document.getElementById("btn1").addEventListener("click", rememberMath);
+
+// function rememberMath() {
+//   Math.random(10000);
+//   alert(Math.random(10000));
+// }
+
+// document.getElementById("btn2").addEventListener("click", dragonList);
+
+// function dragonList() {
+//   document.getElementById("btn2").innerHTML = "Draco, Mushu, Falkor";
+// }
+
+// document.getElementById("btn3").addEventListener("click", colorText);
+
+// function colorText() {
+//   document.getElementById("btn3").style.color = "#32612";
+//   document.getElementById("btn3").style.border = "2px solid green";
+// }
+
+document.getElementById("btn1").addEventListener("click", second);
+
+function second() {
+  alert(`Second times a charm.`);
+}
+
+document.getElementById("btn2").addEventListener("click", needle);
+
+function needle() {
+  alert(`Needle in a haystack`);
+}
+
+document.getElementById("btn3").addEventListener("click", far);
+
+function far() {
+  alert(`So far so good`);
+}
+
+document.getElementById("btn4").addEventListener("click", best);
+
+function best() {
+  alert(`The best of both worlds.`);
+}
+
+document.getElementById("btn5").addEventListener("click", cost);
+
+function cost() {
+  alert(`Cost and arm and leg`);
+}
